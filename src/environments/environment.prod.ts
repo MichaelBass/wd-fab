c0f73970811663d5558df51271140da184664029
MichaelBass <bass_michael@hotmail.com>
2024-07-16 11:19:57 -0500

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  name: 'aws',
-  api:'http://0.0.0.0:3000',
-  stitch_application:'bu-pilotapp-nbbwz',
-  stitch_apiKey:'DeJmI8UAcG9tWUqMDppkQG7uyoPQn91X8dgKaSTwX9BwRzYA6pfAn5QyQQxZWlFz'
-};
